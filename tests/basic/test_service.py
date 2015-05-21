e66c21431f868ddfa9d58473adad4f7ee2083fce
Bjorn Tillenius <bjorn@tillenius.me>
2015-05-21 10:44:37 +0300

    Add integration tests.

--- tests/basic/test_service.py
+++ tests/basic/test_service.py
@@ -117,6 +117,28 @@ class ServiceTest(IntegrationTest):
             "/etc/ssl/certs/landscape_server_ca.crt")
         self.assertTrue(ssl_cert.startswith("-----BEGIN CERTIFICATE-----"))
 
+    def test_pause_resume(self):
+        """
+        A landscape unit can be paused to stop all services and then
+        resumed to start all services.
+        """
+        result = self.environment.pause_landscape()
+        self.assertEqual("completed", result["status"])
+        service_status = self.environment.get_landscape_services_status()
+        # All Landcape services and cron have been stopped
+        self.assertEqual([], service_status["running"])
+        self.assertTrue(len(service_status["stopped"]) > 0)
+        self.assertFalse(
+            self.environment.is_cron_running("landscape-server/0"))
+
+        result = self.environment.resume_landscape()
+        self.assertEqual("completed", result["status"])
+        service_status = self.environment.get_landscape_services_status()
+        # All Landcape services and cron have been started
+        self.assertEqual([], service_status["stopped"])
+        self.assertTrue(len(service_status["running"]) > 0)
+        self.assertTrue(self.environment.is_cron_running("landscape-server/0"))
+
 
 class CronTest(IntegrationTest):
     """Host all the tests that expects the cron daemon to be stopped.
