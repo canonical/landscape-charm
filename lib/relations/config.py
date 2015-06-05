30b148b4a31338a6f73085b3f16f1c4a519a6fce
Danilo Šegan <danilo@canonical.com>
2015-06-05 10:41:37 +0200

    Move relations.config module to CharmError.

---
+++ lib/relations/config.py
@@ -0,0 +1,27 @@
+from charmhelpers.core import hookenv
+from lib.error import CharmError
+from lib.utils import is_valid_url
+
+
+class ConfigError(CharmError):
+    """Charm configuration problem."""
+
+
+class ConfigRequirer(dict):
+    """Dependency manager for the service configuration.
+
+    Take care of validating and exposing configuration values for use
+    in service manager callbacks."""
+
+    def __init__(self, hookenv=hookenv):
+        config = hookenv.config()
+        self._validate(config)
+        self.update({"config": config})
+
+    def _validate(self, config):
+        root_url = config.get("root-url")
+        if root_url and not is_valid_url(root_url):
+            raise ConfigError(
+                "The 'root-url' configuration value is not a valid URL."
+                " Please make sure it is of the form"
+                " 'http[s]://<hostname>/'")
