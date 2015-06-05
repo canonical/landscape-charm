afd4817046fcdae06a233eb25dddc4d74820054d
Danilo Šegan <danilo@canonical.com>
2015-06-05 10:44:36 +0200

    Move relations.hosted module to CharmError.

---
+++ lib/relations/hosted.py
@@ -0,0 +1,41 @@
+from charmhelpers.core.services.helpers import RelationContext
+
+from lib.error import CharmError
+
+DEPLOYMENT_MODES = ("standalone", "edge", "staging", "production")
+
+
+class InvalidDeploymentModeError(CharmError):
+    """Invalid deployment mode."""
+
+
+class HostedRequirer(RelationContext):
+    """Relation data requirer for the 'hosted' interface.
+
+    This relation acquires information from the landscape-hosted subordinate,
+    which will affect local configuration (for instance 'deployment-mode').
+
+    If we're not related to any landscape-hosted subordinate, then this data
+    manager will simply fall back to stock data setting a 'standalone' mode.
+    """
+    name = "hosted"
+    interface = "landscape-hosted"
+    required_keys = [
+        "deployment-mode",  # Can be standalone/edge/staging/production.
+    ]
+
+    def get_data(self):
+        super(HostedRequirer, self).get_data()
+        data = self.get(self.name)
+        if data is None:
+            # This means that we're not currently related to landscape-hosted,
+            # so we set the deployment mode to standalone.
+            self.update({self.name: [{"deployment-mode": "standalone"}]})
+        else:
+            # We're related to landscape-hosted, and it's safe to assume that
+            # there's exactly one unit we're related to, since landscape-hosted
+            # is a subordinate charm.
+            deployment_mode = data[0]["deployment-mode"]
+            if deployment_mode not in DEPLOYMENT_MODES:
+                raise InvalidDeploymentModeError(
+                    "Invalid deployment-mode '%s'" % deployment_mode)
