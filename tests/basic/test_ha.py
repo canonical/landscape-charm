08b98a3113a71b405e675ab3dd5b0b8c3024bedc
Bjorn Tillenius <bjorn@tillenius.me>
2015-06-09 11:32:39 +0300

    add tests for ensuring that Landscape services are HA.

--- tests/basic/test_ha.py
+++ tests/basic/test_ha.py
@@ -6,20 +6,85 @@ from layers import TwoLandscapeUnitsLayer
 
 
 class LandscapeHATest(IntegrationTest):
+    """Test HA aspects of Landscape server.
+
+    The tests in here are intented to test that the haproxy config is
+    correct, so that request gets routed to working units if some of the
+    units go down.
+    """
 
     layer = TwoLandscapeUnitsLayer
 
-    def test_app(self):
-        """Verify that the APP service is up.
-
-        Specifically that it is reachable and that it presents the new
-        user form.
-
-        Note: In order to work on a new server or a server with the
-          first admin user already created, this phrase should match
-          the new-standalone-user form, the login form, and not
-          the maintenance page.
+    def test_app_leader_down(self):
         """
-        self.environment.pause_landscape(unit=0)
-        self.addCleanup(self.environment.resume_landscape, unit=0)
-        self.environment.check_url("/", "passphrase")
+        Verify that the APP service is up when the leader unit goes down.
+        """
+        self.environment.stop_landscape_service("landscape-appserver", unit=0)
+        self.environment.check_service("appserver", attempts=25, interval=1)
+
+    def test_app_non_leader_down(self):
+        """
+        Verify that the APP service is up when a non-leader unit goes down.
+        """
+        self.environment.stop_landscape_service("landscape-appserver", unit=1)
+        self.environment.check_service("appserver", attempts=25, interval=1)
+
+    def test_msg_leader_down(self):
+        """
+        Verify that the MSG service is up when the leader unit goes down.
+        """
+        self.environment.stop_landscape_service("landscape-msgserver", unit=0)
+        self.environment.check_service("msgserver", attempts=25, interval=1)
+
+    def test_msg_non_leader_down(self):
+        """
+        Verify that the MSG service is up when a non-leader unit goes down.
+        """
+        self.environment.stop_landscape_service("landscape-msgserver", unit=1)
+        self.environment.check_service("msgserver", attempts=25, interval=1)
+
+    def test_ping_leader_down(self):
+        """
+        Verify that the PING service is up when the leader unit goes down.
+        """
+        self.environment.stop_landscape_service("landscape-pingserver", unit=0)
+        self.environment.check_service("pingserver", attempts=25, interval=1)
+
+    def test_ping_non_leader_down(self):
+        """
+        Verify that the PING service is up when a non-leader unit goes down.
+        """
+        self.environment.stop_landscape_service("landscape-pingserver", unit=1)
+        self.environment.check_service("pingserver", attempts=25, interval=1)
+
+    def test_api_leader_down(self):
+        """
+        Verify that the API service is up when the leader unit goes down.
+        """
+        self.environment.stop_landscape_service("landscape-api", unit=0)
+        self.environment.check_service("api", attempts=25, interval=1)
+
+    def test_api_non_leader_down(self):
+        """
+        Verify that the API service is up when a non-leader unit goes down.
+        """
+        self.environment.stop_landscape_service("landscape-api", unit=1)
+        self.environment.check_service("api", attempts=25, interval=1)
+
+    def test_upload_leader_down(self):
+        """
+        If the leader goes down, the package upload service goes down as
+        well. We don't have shared storage yet, so the package upload
+        service is run on the leader only.
+        """
+        self.environment.stop_landscape_service("landscape-package-upload")
+        self.environment.check_service("package-upload", state="down")
+
+    def test_upload_non_leader_down(self):
+        """
+        If a non-leader unit goes down, the package upload service goes
+        continues to work, since the package upload service is running
+        on the leader.
+        """
+        self.environment.stop_landscape_service("landscape-package-upload")
+        self.environment.check_service("package-upload", state="down")
