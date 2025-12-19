5056ddc14b175efa3f2ec8abd2e83161452f82bc
jansdhillon <jan.dhillon@canonical.com>
2025-12-19 11:32:19 -0700

    update cert lib, rm observe tls cert available hook

--- src/charm.py
+++ src/charm.py
@@ -367,11 +367,6 @@ class LandscapeServerCharm(CharmBase):
             ],
         )
 
-        self.framework.observe(
-            self.lb_certificates.on.certificate_available,
-            self._on_certificate_available,
-        )
-
     def _get_certificate_request_attributes(
         self,
     ) -> CertificateRequestAttributes | None:
@@ -1138,9 +1133,6 @@ class LandscapeServerCharm(CharmBase):
                 ),
             }
         )
-
-    def _on_certificate_available(self, _) -> None:
-        self._update_haproxy()
 
     def _update_haproxy(self) -> None:
         peer_ips = self.peer_ips
