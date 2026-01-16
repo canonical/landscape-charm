f21c51b80c681eb6a728a5fb9fe315ccc131704c
jansdhillon <jan.dhillon@canonical.com>
2026-01-16 02:20:04 -0700

    rm https-ingress from relate to lbaas scirpt, dont commit design doc

--- bundle-examples/internal-haproxy/relate-to-lbaas.sh
+++ bundle-examples/internal-haproxy/relate-to-lbaas.sh
@@ -10,6 +10,5 @@ juju consume "${OFFER_NAME}" -m "${MODEL_NAME}" "${OFFER_APP_NAME}"
 
 # Integrate it with the Ingress Configurators
 juju integrate -m "${MODEL_NAME}" "$OFFER_APP_NAME:haproxy-route" "http-ingress:haproxy-route"
-juju integrate -m "${MODEL_NAME}" "$OFFER_APP_NAME:haproxy-route" "https-ingress:haproxy-route"
 juju integrate -m "${MODEL_NAME}" "$OFFER_APP_NAME:haproxy-route" "hostagent-messenger-ingress:haproxy-route"
 juju integrate -m "${MODEL_NAME}" "$OFFER_APP_NAME:haproxy-route" "ubuntu-installer-attach-ingress:haproxy-route"
