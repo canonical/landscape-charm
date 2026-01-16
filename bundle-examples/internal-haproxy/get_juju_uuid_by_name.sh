4c6f6950be7bf17de0c73ecb48b70576b2547dfc
jansdhillon <jan.dhillon@canonical.com>
2026-01-15 17:21:22 -0700

    fix model input reading

--- bundle-examples/internal-haproxy/get_juju_uuid_by_name.sh
+++ bundle-examples/internal-haproxy/get_juju_uuid_by_name.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-_MODEL_NAME=${MODEL_NAME:-"landscape-charm-build"}
-MODEL_UUID=$(juju models --format json | jq -r  --arg model_name "$_MODEL_NAME" '.models[] | select(."short-name" == $model_name) | ."model-uuid"')
+input=$(cat)
+_model_name=$(echo "$input" | jq -r '.model_name')
+MODEL_NAME=${_model_name:-"landscape-charm-build"}
+MODEL_UUID=$(juju models --format json | jq -r  --arg model_name "$MODEL_NAME" '.models[] | select(."short-name" == $model_name) | ."model-uuid"')
 
 echo "{\"uuid\": \"$MODEL_UUID\"}"
