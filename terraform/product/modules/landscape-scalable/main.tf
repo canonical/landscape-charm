d7714c87546ad45023bd0fe56fc53e12f535f9f3
jansdhillon <jan.dhillon@canonical.com>
2026-01-20 19:23:42 -0700

    fix product module source

--- terraform/product/modules/landscape-scalable/main.tf
+++ terraform/product/modules/landscape-scalable/main.tf
@@ -1,8 +1,7 @@
 # © 2025 Canonical Ltd.
 
 module "landscape_server" {
-  # TODO: Pin to real release when we get one with these fixes
-  source      = "git::https://github.com/jansdhillon/landscape-charm.git//terraform?ref=merge-product-module"
+  source      = "../../../charm"
   model       = var.model
   config      = var.landscape_server.config
   app_name    = var.landscape_server.app_name
